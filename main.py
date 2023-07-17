d3f1d17aeaf40b98ea6982d1d3ab10c74f78bcd0
fractal161 <628snapdragon@gmail.com>
2023-07-17 18:47:34 -0500

    write date of most recent fetch to config

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from configparser import ConfigParser
+from datetime import datetime
 import os
 import urllib.request
 
@@ -11,15 +12,20 @@ if not os.path.exists('config.toml'):
 config = ConfigParser()
 config.read('config.toml')
 
-spreadsheetid = config['sheet']['spreadsheetid']
-sheetid = config['sheet']['sheetid']
+spreadsheet_id = config['sheet']['spreadsheet_id']
+sheet_id = config['sheet']['sheet_id']
 
-db_url  = f'https://docs.google.com/spreadsheets/d/{spreadsheetid}/export?format=csv&gid={sheetid}'
-print('Connecting to ...')
+db_url = f'https://docs.google.com/spreadsheets/d/{spreadsheet_id}/export?format=csv&gid={sheet_id}'
+print('Establishing connection to Google Sheets...')
 # TODO: show download progress if possible
 # Idea: https://stackoverflow.com/a/1517728
+update_time = datetime.now()
 with urllib.request.urlopen(db_url) as response:
     print('Success! Writing database to file...')
-    print(response.headers)
     with open('matchdb/db.csv', 'wb') as f:
         f.write(response.read())
+
+# update config with update time
+config['db']['last_update_time'] = update_time.isoformat()
+with open('config.toml', 'w') as config_file:
+    config.write(config_file)
